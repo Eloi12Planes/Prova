ac7e04f3d36507e64f546360003bb81274ed4f56
hola_ <hola_@Razer_Eloi>
2022-12-19 16:56:19 +0100

    Caambiar valor n

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,12 @@
 public class Main {
 
 	public static void main(String[] args) {
-		int n =  0;
+		int n =  10;
 		n++;
 		
 		if(n % 2==0) {			
 			System.out.println("Num "+n);
-		}
-		
+		}	
 		
 	}
 	
